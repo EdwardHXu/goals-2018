d8b4a73de92501de39dd8e21502f43641163d23e
EdwardXu <edwardxu@bu.edu>
2018-01-22 16:11:30 -0500

    modify schedule.go

--- src/mapreduce/schedule.go
+++ src/mapreduce/schedule.go
@@ -6,10 +6,12 @@ import "fmt"
 func (mr *Master) schedule(phase jobPhase) {
 	var ntasks int
 	var nios int // number of inputs (for reduce) or outputs (for map)
+	isMapPhase := false
 	switch phase {
 	case mapPhase:
 		ntasks = len(mr.files)
 		nios = mr.nReduce
+		isMapPhase = true
 	case reducePhase:
 		ntasks = mr.nReduce
 		nios = len(mr.files)
@@ -22,7 +24,35 @@ func (mr *Master) schedule(phase jobPhase) {
 	// Remember that workers may fail, and that any given worker may finish
 	// multiple tasks.
 	//
-	// TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO TODO
+	var taskQueue = []DoTaskArgs{}
+	for t := 0; t < ntasks; t++ {
+		task := DoTaskArgs{mr.jobName, "", phase, t, nios}
+		if isMapPhase {
+			task.File = mr.files[t]
+		}
+		taskQueue = append(taskQueue, task)
+	}
+	completed := make(chan int)
+	for len(taskQueue) > 0 {
+		go func(task DoTaskArgs) {
+			for w := range mr.registerChannel {
+				if call(w, "Worker.DoTask", &task, new(struct{})) {
+					completed <- 0
+					mr.registerChannel <- w
+					break
+				}
+				go func() {
+					mr.registerChannel <- w
+				}()
+			}
+		}(taskQueue[0])
+		taskQueue = taskQueue[1:]
+	}
+	for i := 0; i < ntasks; i++ {
+		<-completed
+		fmt.Println("i:%d", i)
+	}
+	//
 	//
 	fmt.Printf("Schedule: %v phase done\n", phase)
 }
